ea2ae4dbb42a14c91ec574ba6ca7ff6b124399b9
margocat57 <bin242717@gmail.com>
2025-12-02 02:58:47 +0300

    refactored op_func

--- src/core/operator_func.h
+++ src/core/operator_func.h
@@ -39,21 +39,21 @@ struct op_info{
 
 
 const op_info OPERATORS_INFO[] = {
-    {OPERATORS::INCORR},
-    {OPERATORS::OP_ADD, "+",   1, "OP_ADD", 1,  "" ,       " + ",      "",  DIFFERENCIATOR(DiffAdd,) CALCUL(CalcAdd)},
-    {OPERATORS::OP_SUB, "-",   1, "OP_SUB", 1,  "" ,       " - ",      "",  DIFFERENCIATOR(DiffSub,) CALCUL(CalcSub)},
-    {OPERATORS::OP_MUL, "*",   1, "OP_MUL", 2,  "" ,       " \\cdot ", "",  DIFFERENCIATOR(DiffMul,) CALCUL(CalcMul)},
-    {OPERATORS::OP_DIV, "/",   1, "OP_DIV", 2,  "\\frac{", "}{",       "}", DIFFERENCIATOR(DiffDiv,) CALCUL(CalcDiv)},
-    {OPERATORS::OP_DEG, "^",   1, "OP_DEG", 2,  "",        "^{",      "}",DIFFERENCIATOR(DiffDeg,) CALCUL(CalcDeg)},
-    {OPERATORS::OP_SIN, "sin", 3, "OP_SIN", 2,  "\\sin(" , NULL ,      ")", DIFFERENCIATOR(DiffSin,) CALCUL(CalcSin)},
-    {OPERATORS::OP_COS, "cos", 3, "OP_COS", 2,  "\\cos(" , NULL ,      ")", DIFFERENCIATOR(DiffCos,) CALCUL(CalcCos)},
-    {OPERATORS::OP_LN,  "ln",  2, "OP_LN",  2,  "\\ln(" ,  NULL ,      ")", DIFFERENCIATOR(DiffLn,)  CALCUL(CalcLn) },
-    {OPERATORS::OP_TG,  "tg",  2, "OP_TG",  2,  "\\tan(" , NULL ,      ")", DIFFERENCIATOR(DiffTg,)  CALCUL(CalcTg) },
-    {OPERATORS::OP_CTG, "ctg", 3, "OP_CTG", 2,  "\\cot(" , NULL ,      ")", DIFFERENCIATOR(DiffCtg,) CALCUL(CalcCtg)},
-    {OPERATORS::OP_SH,  "sh",  2, "OP_SH",  2,  "\\sinh(", NULL ,      ")", DIFFERENCIATOR(DiffSh,)  CALCUL(CalcSh) },
-    {OPERATORS::OP_CH,  "ch",  2, "OP_CH",  2,  "\\cosh(", NULL ,      ")", DIFFERENCIATOR(DiffCh,)  CALCUL(CalcCh) },
-    {OPERATORS::OP_TH,  "th",  2, "OP_TH",  2,  "\\tanh(", NULL ,      ")", DIFFERENCIATOR(DiffTh,)  CALCUL(CalcTh) },
-    {OPERATORS::OP_CTH, "cth", 3, "OP_CTH", 2,  "\\coth(", NULL ,      ")", DIFFERENCIATOR(DiffCth,) CALCUL(CalcCth)}
+    {INCORR},
+    {OP_ADD, "+",   1, "OP_ADD", 1,  "" ,       " + ",      "",  DIFFERENCIATOR(DiffAdd,) CALCUL(CalcAdd)},
+    {OP_SUB, "-",   1, "OP_SUB", 1,  "" ,       " - ",      "",  DIFFERENCIATOR(DiffSub,) CALCUL(CalcSub)},
+    {OP_MUL, "*",   1, "OP_MUL", 2,  "" ,       " \\cdot ", "",  DIFFERENCIATOR(DiffMul,) CALCUL(CalcMul)},
+    {OP_DIV, "/",   1, "OP_DIV", 2,  "\\frac{", "}{",       "}", DIFFERENCIATOR(DiffDiv,) CALCUL(CalcDiv)},
+    {OP_DEG, "^",   1, "OP_DEG", 2,  "",        "^{",       "}", DIFFERENCIATOR(DiffDeg,) CALCUL(CalcDeg)},
+    {OP_SIN, "sin", 3, "OP_SIN", 2,  "\\sin(" , NULL ,      ")", DIFFERENCIATOR(DiffSin,) CALCUL(CalcSin)},
+    {OP_COS, "cos", 3, "OP_COS", 2,  "\\cos(" , NULL ,      ")", DIFFERENCIATOR(DiffCos,) CALCUL(CalcCos)},
+    {OP_LN,  "ln",  2, "OP_LN",  2,  "\\ln(" ,  NULL ,      ")", DIFFERENCIATOR(DiffLn,)  CALCUL(CalcLn) },
+    {OP_TG,  "tg",  2, "OP_TG",  2,  "\\tan(" , NULL ,      ")", DIFFERENCIATOR(DiffTg,)  CALCUL(CalcTg) },
+    {OP_CTG, "ctg", 3, "OP_CTG", 2,  "\\cot(" , NULL ,      ")", DIFFERENCIATOR(DiffCtg,) CALCUL(CalcCtg)},
+    {OP_SH,  "sh",  2, "OP_SH",  2,  "\\sinh(", NULL ,      ")", DIFFERENCIATOR(DiffSh,)  CALCUL(CalcSh) },
+    {OP_CH,  "ch",  2, "OP_CH",  2,  "\\cosh(", NULL ,      ")", DIFFERENCIATOR(DiffCh,)  CALCUL(CalcCh) },
+    {OP_TH,  "th",  2, "OP_TH",  2,  "\\tanh(", NULL ,      ")", DIFFERENCIATOR(DiffTh,)  CALCUL(CalcTh) },
+    {OP_CTH, "cth", 3, "OP_CTH", 2,  "\\coth(", NULL ,      ")", DIFFERENCIATOR(DiffCth,) CALCUL(CalcCth)}
 };
 
 #endif //OPERATOR_FUNC_H
